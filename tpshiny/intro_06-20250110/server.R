255d6c1e99f1d47745a9e6d2f8e28b642f9912e0
VinyPrestyNakavoua <nakavouavinypresty@gmail.com>
2025-01-14 10:58:22 +0100

    last commit : fin exo6, app6

--- tpshiny/intro_06-20250110/server.R
+++ tpshiny/intro_06-20250110/server.R
@@ -1,28 +1,54 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
 library(ggplot2)
 
-# Define server logic required to draw a histogram
+# Define server logic required to draw appropriate graphs
 function(input, output, session) {
-
+  
+  ## Choisir le dataset
   dataset <- reactive({
-    get(input$dataset, "package:ggplot2")
+    switch(input$dataset,
+           "economics" = ggplot2::economics,
+           "faithfuld" = ggplot2::faithfuld,
+           "seals" = ggplot2::seals
+    )
+  })
+  
+  ## Choisir le choix dynamique
+  output$dynamicinput <- renderUI({
+    data <- dataset()  
+    selectable_columns <- setdiff(names(data), "date") # Exclude the "date" column, we cannot plot it
+    selectInput(
+      inputId = "dynamicselect",
+      label = "Choisissez la variable :",
+      choices = selectable_columns
+    )
   })
   
   output$summary <- renderPrint({
-    summary(dataset())
+    data <- dataset()
+    if (!is.null(input$dynamicselect)) {
+      summary(data[[input$dynamicselect]])
+    }
   })
   
   output$le_graphe <- renderPlot({
-    plot(dataset())
+    data <- dataset()
+    if (!is.null(input$dynamicselect)) {
+      variable <- data[[input$dynamicselect]]
+      
+      if (is.numeric(variable)) {
+        # Histogram for quantitative variables
+        ggplot(data, aes(x = .data[[input$dynamicselect]])) +
+          geom_histogram(binwidth = diff(range(variable)) / input$bins, fill = input$color, color = "white") +
+          labs(title = paste("Histogramme de", input$dynamicselect), x = input$dynamicselect, y = "Fréquence") +
+          theme_minimal()
+      } else {
+        # Bar plot for qualitative variables
+        ggplot(data, aes(x = factor(.data[[input$dynamicselect]]))) +
+          geom_bar(fill = input$color, color = "black") +
+          labs(title = paste("Diagramme en barres de", input$dynamicselect), x = input$dynamicselect, y = "Nombre") +
+          theme_minimal()
+      }
+    }
   }, res = 96)
-
 }
--- tpshiny/intro_06-20250110/ui.R
+++ tpshiny/intro_06-20250110/ui.R
@@ -12,14 +12,30 @@ library(ggplot2)
 
 # Define UI for application that draws a histogram
 fluidPage(
-  titlePanel("Visualisation des jeux de données de ggplot2"), 
+  titlePanel(
+    div(
+      style = "display: flex; align-items: center;",
+      h1("Visualisation des jeux de données de ggplot2"),
+      img(src = "Logo-Normal-SD.png", height = "100px", style = "margin-left: 15px;")
+    )
+  ),
   sidebarLayout(
     sidebarPanel( 
       selectInput("dataset", "Dataset", choices = c("economics", "faithfuld", "seals")),
+      uiOutput("dynamicinput"),
+      sliderInput(inputId="bins", label = "Nombre de classes", min=2, max=30, value=10),
+      radioButtons("color", "Couleur de l'histogramme", 
+                   choices = c("Bleu" = "blue", 
+                               "Vert" = "green", 
+                               "Rouge" = "red"), 
+                   selected = "blue")
       
       ),
     
+    
+    
     mainPanel(
+      
       verbatimTextOutput("summary"),
       plotOutput("le_graphe")
     )
